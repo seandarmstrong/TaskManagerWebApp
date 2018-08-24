a0fb740731e3584627d1bf23415c8ecb6e79a87c
sean <armstrong.d.sean@gmail.com>
2018-08-24 13:42:21 -0400

    first commit of project

---
+++ TaskList.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Domain.Models
+{
+    class User
+    {
+    }
+}
