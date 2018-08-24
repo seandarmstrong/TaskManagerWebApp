d04080921e1dd1e4270137d13aea0f24501f5cc7
sean <armstrong.d.sean@gmail.com>
2018-08-24 15:33:44 -0400

    set up code first model and DB is created

---
+++ TaskList.Data/Maps/TaskMap.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using TaskList.Domain.Models;
+
+namespace TaskList.Data.Maps
+{
+    class TaskMap : EntityTypeConfiguration<Task>
+    {
+        public TaskMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
---
+++ TaskList.Data/TaskListInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskList.Data;
+using System.Data.Entity;
+using TaskList.Domain.Models;
+
+namespace TaskList.Data
+{
+    class TaskListInitializer : DropCreateDatabaseAlways<TaskListContext>
+    {
+        protected override void Seed(TaskListContext context)
+        {
+
+        }
+    }
+}
